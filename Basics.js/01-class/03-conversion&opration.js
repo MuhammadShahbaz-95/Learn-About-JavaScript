5ce3158b32b45d23cfd78be5bdfadd3a47daa0fc
Muhammad Shahbaz <157780375+MuhammadShahbaz-95@users.noreply.github.com>
2024-03-21 23:26:02 +0000

    conversion of data type

---
+++ Basics.js/01-class/03-conversion&opration.js
@@ -0,0 +1,19 @@
+let score = "33abc"
+
+let valueInNumber =Number(score)
+console.log(typeof valueInNumber)
+console.log(valueInNumber)
+
+//conversion
+//"33"=33
+//"333abc"= NaN
+//true =1, false =0
+
+let isLogedIn =1
+
+let booleanIsLogedIn = Boolean(isLogedIn)
+console.log(booleanIsLogedIn);
+//convert boolean value
+ // "number con to boolean"  1 con true, 0 con flase
+ //"string con to boolean"   false = "",  true = "shahbaz",  
+
